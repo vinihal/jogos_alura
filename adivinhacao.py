cd0ca6d0a9761f8534e8154d64734bd8b2fcf29c
vinihal <vinifromthefuture@gmail.com>
2022-02-17 20:34:02 -0300

    ponto de exclamação

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,7 +3,7 @@ import random
 def jogar():
 
     print("*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
+    print("Bem vindo ao jogo de Adivinhação!!")
     print("*********************************")
 
     numero_secreto = random.randrange(1,101)
